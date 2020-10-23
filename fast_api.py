78acd181f03b02fb83ef824958734a436bf06f02
ofir-bar <ofirbar5@gmail.com>
2020-10-23 22:52:44 +0300

    code clean

--- fast_api.py
+++ fast_api.py
@@ -1,7 +1,7 @@
 from typing import Optional
-
 from fastapi import FastAPI
 
+# region API Initialization
 tags_metadata = [
     {
         "name": "metadata",
@@ -30,37 +30,47 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# endregion
+
+# region Companies
 
 # Output the company CIK
 @app.get("/{exchange_name}/{stock_ticker}/company_cik", tags=['companies'])
-def read_root(exchange_name: str, stock_ticker: str):
+def get_company_cik(exchange_name: str, stock_ticker: str):
     return {f"{exchange_name}: {stock_ticker}"}
-
-
-@app.get("/contributors", tags=['metadata'])
-def read_root():
-    return {"Trex Team"}
 
 
 # Output the details of the last submitted filing for {STOCK_TICKER} at {year}.
 @app.get("/{exchange_name}/{stock_ticker}/get_last_filling_for_company_in_year", tags=['companies'])
-def read_get_last_filling_for_company_in_year(exchange_name: str, stock_ticker: str,
+def get_last_filling(exchange_name: str, stock_ticker: str,
                                               filling_year: Optional[int] = None):
     return {f"{exchange_name}: {stock_ticker}, year: {filling_year}"}
 
 
 # Returns true if there is a new filling for this company.
 @app.get("/{exchange_name}/{stock_ticker}/is_there_new_filling", tags=['companies'])
-def read_is_there_new_filling(exchange_name: str, stock_ticker: str):
+def is_there_new_filling(exchange_name: str, stock_ticker: str):
     return {f"{exchange_name}: {stock_ticker}"}
 
 
 # Writes the last time we checked the company filling to some DB.
 @app.post("/{exchange_name}/{stock_ticker}/last_time_checked_for_company_filling", tags=['companies'])
-def read_is_there_new_filling(exchange_name: str, stock_ticker: str):
+def post_last_filling_time_for_company(exchange_name: str, stock_ticker: str):
     return {f"{exchange_name}: {stock_ticker}"}
 
 
+# endregion
+
+# region metadata
+@app.get("/contributors", tags=['metadata'])
+def get_list_of_contributors():
+    return {"Trex Team"}
+
+
+# endregion
+
+# region Testing
 @app.get("/items/{item_id}", tags=['testing'])
-def read_hello_world():
+def print_hello_world():
     return {"Hello world!"}
+# endregion
